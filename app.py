8487848dad942eb231f2d64afc9efe7094b59da0
Kaustubh Olpadkar <kaustubh.olpadkar@gmail.com>
2018-07-06 11:41:15 +0530

    upload source

---
+++ app.py
@@ -0,0 +1,14 @@
+
+def main ():
+
+	print('Enter First Number : ', end="")
+	first_number = int(input())
+	print('Enter Second Number : ', end="")
+	second_number = int(input())
+
+	total = first_number + second_number
+
+	print('Addition of two numbers is ' + str(total))
+
+if __name__ == '__main__':
+	main()
